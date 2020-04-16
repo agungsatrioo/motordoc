cc4f2a7df78c71757a928f58399cbc408268517b
zanuarts <nuartea27@gmail.com>
2020-04-17 01:45:28 +0700

    add login page

--- lib/src/common/splash.dart
+++ lib/src/common/splash.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:motordoc/src/page/home.dart';
+import 'package:motordoc/src/page/login/login.dart';
 
 class SplashScreen extends StatefulWidget{
   @override
@@ -17,11 +17,11 @@ class _SplashScreen extends State<SplashScreen>{
   }
 
   startSplashScreen()async{
-    var duration = const Duration(seconds: 3);
+    var duration = const Duration(seconds: 5);
     return Timer(duration, (){
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (_){
-          return HomePage();
+          return LoginPage();
         })
       );
     });
@@ -30,12 +30,50 @@ class _SplashScreen extends State<SplashScreen>{
   @override
   Widget build (BuildContext context){
     return Scaffold(
-      backgroundColor : Colors.blueAccent,
+      backgroundColor : Color(0xFF3688DE),
       body: Center(
-        child: Image.asset(
-          "assets/images/logo.png",
-          width : 250.0,
-          height: 250.0,
+        child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 203,left: 67, right: 67),
+            child: Image.asset(
+              "assets/images/logo.png",
+              width : 250.0,
+              height: 250.0,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: Text(
+              "MOTORDOC",
+              style: TextStyle(
+                fontSize:36.0,
+                fontWeight: FontWeight.normal,
+                color: Color(0xFFFFFFFF),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 140),
+            child: Text(
+              "V 1.0",
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xFFFFFFFF),
+              )
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 1),
+            child: Text(
+                "Developed by Group 1 PPL",
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xFFFFFFFF),
+                )
+            ),
+          ),
+        ],
         ),
       ),
     );
---
+++ lib/src/page/login/login.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginPage extends StatefulWidget{
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        resizeToAvoidBottomPadding: false,
+      body: Container(
+        padding: const EdgeInsets.only(right: 38, left: 38),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 109),
+                child: Image.asset(
+                  "assets/images/logo.png",
+                  width : 113.0,
+                  height: 113.0,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 38),
+                  child: Text(
+                    "Masukan nomor telepon",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 2),
+                    child: Text(
+                      "Untuk memulai",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  )
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  WhitelistingTextInputFormatter.digitsOnly
+                ], // Only numbers can be entered
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 223),
+                child: Text(
+                  "Dengan mendaftar, \nsaya akan menerima Syarat dan Ketentuan Pengguna yang berlaku di Motordoc.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 11,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 11),
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(7.0),
+                  ),
+                  color: Color(0xFF3688DE),
+                  textColor: Colors.white,
+                  padding: EdgeInsets.only(right: 115, left: 115, top: 9, bottom: 9),
+                  onPressed: () {},
+                  child: const Text(
+                      'VERIFIKASI',
+                      style: TextStyle()
+                  ),
+                ),
+              )
+            ],
+        ),
+      )
+    );
+  }
+}
