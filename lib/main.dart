3fac860ee905b31d133126159029c91f71b557eb
ugang <manggung@qmail.id>
2020-04-29 12:43:30 +0700

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'src/ui/pages/page_splash.dart';
+import 'src/utils/util_theme.dart';
 
 void main(){
   runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Motordoc',
-        theme: ThemeData.light().copyWith(
-            buttonTheme: ButtonThemeData(
-                colorScheme: ColorScheme(
-                  primary: Color(0xFF3688DE),
-                  onPrimary: Colors.white,
-                  primaryVariant: Color(0xFF3688DE),
-                  secondary: Color(0xff4c9ee7),
-                  secondaryVariant: Color(0xff4c9ee7),
-                  onSecondary: Colors.white,
-                  error: Color(0xffdc3545),
-                  onError: Colors.white,
-                  surface: Color(0xfff8f9fa),
-                  onSurface: Colors.black,
-                  background: Color(0xfff8f9fa),
-                  onBackground: Colors.black,
-                  brightness: Brightness.light,
-                )
-            )
-        ),
+        theme: MotorDocTheme,
         home: SplashScreen(),
       )
   );
---
+++ lib/src/utils/util_theme.dart
@@ -0,0 +1,50 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+final Color primary_logo = Color(0xFF3688DE);
+
+final Color primary = Color(0xff1890ff);
+final Color link = Color(0xff1890ff);
+final Color success = Color(0xff52c41a);
+final Color warning = Color(0xfffaad14);
+final Color error = Color(0xfff5222d);
+
+final Color headingText = Color.fromRGBO(0, 0, 0, .85);
+final Color mainText = Color.fromRGBO(0, 0, 0, .65);
+final Color secondaryText = Color.fromRGBO(0, 0, 0, .45);
+final Color disabledText = Color.fromRGBO(0, 0, 0, .25);
+
+final Color mainBorder = Color(0xffd9d9d9);
+
+final double fontSizeBase = 14.0;
+
+final double radiusBase = 4;
+
+// ignore: non_constant_identifier_names
+ThemeData get MotorDocTheme => ThemeData.from(
+      colorScheme: ColorScheme(
+          primary: primary_logo,
+          primaryVariant: primary,
+          secondary: Color(0xff468ddb),
+          secondaryVariant: Color(0xff499cea),
+          surface: Colors.white,
+          background: Colors.white,
+          error: error,
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onSurface: Colors.white,
+          onBackground: Colors.white,
+          onError: Colors.white,
+          brightness: Brightness.light),
+    ).copyWith(
+      appBarTheme: AppBarTheme(elevation: 0.0),
+      buttonTheme: ButtonThemeData(
+        padding: EdgeInsets.only(right: 8.0, bottom: 12.0),
+        disabledColor: disabledText,
+        shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(2.0),
+        ),
+      ),
+    );
