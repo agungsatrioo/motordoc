1ba2ee7bcdfc2751fc8d521e5a42c9aa0f9d2da2
ugang <manggung@qmail.id>
2020-05-02 14:04:56 +0700

    revise home layout

--- lib/src/ui/pages/home/home.dart
+++ lib/src/ui/pages/home/home.dart
@@ -7,166 +7,184 @@ import 'package:motordoc/src/ui/widgets/widget_location.dart';
 import 'package:motordoc/src/utils/util_theme.dart';
 
 class HomePage extends StatefulWidget{
-  @override createState() => _MyHomePage();
+  @override createState() => _HomePageState();
 }
 
-class _MyHomePage extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primary_logo,
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Container(
-          child: Column(
-          children: <Widget>[
-            Container(
+      body: CustomScrollView(
+        slivers: <Widget> [
+          SliverAppBar(
+            automaticallyImplyLeading: false,
+            elevation: 0.0,
+            pinned: true,
+            flexibleSpace: SafeArea(
+              child:Container(
+                padding: EdgeInsets.only(top:8.0, left:8.0, right:8.0),
+                child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        MotordocLogo(),
+                        WidgetLocation()
+                      ],
+                    ),
+              ),
+            )
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              color: primary_logo,
+              child: Container(
               padding: EdgeInsets.all(8.0),
               child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      MotordocLogo(),
-                      WidgetLocation()
-                    ],
-                  ),
-                  Container(
-                        decoration: BoxDecoration(
-                          borderRadius: new BorderRadius.circular(7.0),
-                          color: Color (0xFFFFFFFF),
-                        ),
-                        width: 360,
-                        height: 38,
-                        child: Row(
-                            children:<Widget>[
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal:8.0, vertical:2.0),
-                                child: Icon(
-                                  Icons.search,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(7.0),
-                                child: Text(
-                                  "Ada masalah apa dengan motormu?",
-                                  style: TextStyle(
-                                    color: Color (0xFFC1C0C0),
-                                  ),
-                                ),
-                              ),
-                            ]
-                        ),
+              children: <Widget>[
+                Container(
+                      decoration: BoxDecoration(
+                        borderRadius: new BorderRadius.circular(7.0),
+                        color: Color (0xFFFFFFFF),
                       ),
-                      Row(
-                          children: <Widget>[
+                      width: 360,
+                      height: 38,
+                      child: Row(
+                          children:<Widget>[
                             Padding(
-                              padding: const EdgeInsets.only(top: 14, right: 23.0, left: 12.0),
-                              child: Image.asset(
-                                "assets/images/mechanic.png",
-                                height: 115.0,
+                              padding: const EdgeInsets.symmetric(horizontal:8.0, vertical:2.0),
+                              child: Icon(
+                                Icons.search,
                               ),
                             ),
-                            Column(
+                            Padding(
+                              padding: const EdgeInsets.all(7.0),
+                              child: Text(
+                                "Ada masalah apa dengan motormu?",
+                                style: TextStyle(
+                                  color: Color (0xFFC1C0C0),
+                                ),
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+                    Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 14, right: 23.0, left: 12.0),
+                            child: Image.asset(
+                              "assets/images/mechanic.png",
+                              height: 115.0,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              DateWidget(),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5.0),
+                                child: Text(
+                                  "Hai Rifqi Sambas",
+                                  style: TextStyle(
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 30.0,
+                                    fontWeight: FontWeight.w100,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5.0),
+                                child: Text(
+                                  "Ada yang bisa kami bantu?",
+                                  style: TextStyle(
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.w100,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+              ],
+              ),
+            )
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(7.0),
+                    topRight: const Radius.circular(7.0))
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Kategori", style: Theme.of(context).textTheme.subhead),
+                  ),
+                  Container(
+                        height: 75,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 15,
+                          itemBuilder: (BuildContext context, int index) => Card(
+                            child: Center(child: Text('Dummy Card Text')),
+                          ),
+                        ),
+                      ),
+                   Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Sparepart", style: Theme.of(context).textTheme.subhead),
+                  ),
+                  Container(
+                        height: 75,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 15,
+                          itemBuilder: (BuildContext context, int index) => Card(
+                            child: Center(child: Text('Dummy Card Text')),
+                          ),
+                        ),
+                      ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Tahukah Kamu?", style: Theme.of(context).textTheme.subhead),
+                  ),
+                  ListView.builder(
+                    padding: EdgeInsets.zero,
+                    physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 5,
+                      itemBuilder: (ctx,int) {
+                        return Material(
+                          child: InkWell(
+                            child: Container(
+                            padding: EdgeInsets.symmetric(horizontal:8.0, vertical:4.0),
+                            child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                DateWidget(),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 5.0),
-                                  child: Text(
-                                    "Hai Rifqi Sambas",
-                                    style: TextStyle(
-                                      color: Color(0xFFFFFFFF),
-                                      fontSize: 30.0,
-                                      fontWeight: FontWeight.w100,
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 5.0),
-                                  child: Text(
-                                    "Ada yang bisa kami bantu?",
-                                    style: TextStyle(
-                                      color: Color(0xFFFFFFFF),
-                                      fontSize: 12.0,
-                                      fontWeight: FontWeight.w100,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
+                                Text("Motivation ${int+1}", style: Theme.of(context).textTheme.title,),
+                                Text("Lorem ipsum ajaah", style: Theme.of(context).textTheme.subtitle,)
+                              ]
+                            ),
+                          ),
+                          onTap: () => {}
+                          ),
+                        );
+                      },
+                      ),
                 ],
-              ),
+              )
             ),
-            Expanded(
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: new BorderRadius.only(
-                        topLeft: const Radius.circular(7.0),
-                        topRight: const Radius.circular(7.0))
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        'Headline',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Container(
-                        height: 75,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 15,
-                          itemBuilder: (BuildContext context, int index) => Card(
-                            child: Center(child: Text('Dummy Card Text')),
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Headline Mid',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Container(
-                        height: 75,
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 15,
-                          itemBuilder: (BuildContext context, int index) => Card(
-                            child: Center(child: Text('Dummy Card Text')),
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Demo Headline 2',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemBuilder: (ctx,int){
-                            return Card(
-                              child: ListTile(
-                                title: Text('Motivation $int'),
-                                subtitle: Text('this is a description of the motivation')),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-          ],
-          ),
-        )
-      )
+          )
+        ]
+      ),
     );
   }
 }
